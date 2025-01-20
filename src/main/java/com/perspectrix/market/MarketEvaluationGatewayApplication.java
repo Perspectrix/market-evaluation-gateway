4cf74a2f66d66c69e359a1c24be01e42d53279ff
william.stone <will@williamstone.me>
2025-01-20 08:54:20 -0500

    Gateway working.

---
+++ src/main/java/com/perspectrix/market/MarketEvaluationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.perspectrix.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEvaluationGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketEvaluationGatewayApplication.class, args);
+    }
+
+}
